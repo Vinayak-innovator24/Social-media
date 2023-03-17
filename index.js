892ce07f3248ef8a635219d7792aa914054f37cc
Vinayak-innovator24 <vinayak.pande2403@gmail.com>
2023-03-17 22:53:21 +0530

    project done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+const {middleware} = require("./routes/middleware")
+dotenv.config();
+
+// connecting mongodb
+
+mongoose.connect(process.env.MONGO_URL, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+        console.log("Connected to MongoDB")
+);
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", middleware, userRoute);
+app.use("/api/authenticate",  authRoute);
+app.use("/api/posts", middleware, postRoute);
+
+// using express
+app.listen(3000, ()=> {
+    console.log("Connected to backend server")
+});
+
